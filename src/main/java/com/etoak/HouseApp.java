ff82b8899891b4c5563892154e35efac9364bf94
etoak <etoak>
2020-07-02 10:39:40 +0800

    配置

--- src/main/java/com/etoak/HouseApp.java
+++ src/main/java/com/etoak/HouseApp.java
@@ -1,8 +1,16 @@
 package com.etoak;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by 19161 on 2020/7/2.
  */
+@SpringBootApplication
 public class HouseApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseApp.class, args);
+    }
 
 }
